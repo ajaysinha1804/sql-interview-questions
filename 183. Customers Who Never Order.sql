c3176ea470210aa3cf25399d6b0d046f7be62f72
ajaysinha1804 <ajaysinha1804@gmail.com>
2024-06-12 16:01:46 +0530

    modified  second approach for the Q-1 on 12-JUN-2024

--- 183. Customers Who Never Order.sql
+++ 183. Customers Who Never Order.sql
@@ -4,14 +4,17 @@
 -- My Approach   :
 -- Approach 1    :
 -- Firstly combine both the Customers and Orders table and try to find out the Null values in the place of order id value if it is null value then return the customer name from the customer table
--- Approach 2    :
--- Combine both the tables using inner join and extract the customer names from the the table those who have ordered something and exclude those names
--- Finally return the names which are not excluded
+
 SELECT c.name AS Customers
 FROM Customers c
 LEFT JOIN Orders o 
 ON c.id=o.customerId
 WHERE o.id is Null
+
+
+-- Approach 2    :
+-- Combine both the tables using inner join and extract the customer names from the the table those who have ordered something and exclude those names
+-- Finally return the names which are not excluded
 
 SELECT c1.name AS Customers
 FROM Customers c1
